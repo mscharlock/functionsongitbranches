09d799144280d254b502e0664d2b99a69111113b
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-05-14 16:13:24 -0700

    Editing out merging info
    Getting rid of >>>>>HEAD etc from previous merge problems.

--- app.js
+++ app.js
@@ -39,10 +39,6 @@ sum(4, 7);
 //
 // // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 //
-=======
-=======
-
->>>>>>> 22260a7de81c8e8ba540af8411a0d097b91650e1
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
@@ -66,8 +62,6 @@ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
-<<<<<<< HEAD
->>>>>>> problem2
 // /////////////////////////////////////
 // /* Problem 3
 // Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
@@ -90,7 +84,7 @@ testMultiply(5,9);
 // // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 //
 // /////////////////////////////////////
-=======
+
 //Note!! I somehow ended up trying to merge in the command line, which has given me this branch of master|MERGING for the last 10 or so minutes so I'm just editing in the text on the web. 
 
 /* Problem 3
@@ -118,7 +112,6 @@ testSumAndMultiply(4,7,5);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
->>>>>>> 22260a7de81c8e8ba540af8411a0d097b91650e1
 // /* Problem 4
 // Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 //
