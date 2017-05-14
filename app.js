e9959f4df411d9a5cb92609344f11af25ded5f01
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-05-14 16:16:36 -0700

    Editing merging info
    There were still some errors left over from my master|MERGING problem, so fixing

--- app.js
+++ app.js
@@ -14,31 +14,7 @@ function sum(a,b){
   return [mySum, myMsg];
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 sum();
-// Here is the test for sum(); uncomment it to run it
-sum(4, 7);
-
-
-// /////////////////////////////////////
-// /* Problem 2
-// Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
-//
-// "The product of 5 and 9 is 45."
-//
-// Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
-//
-// // Write your code here
-// function multiply(a,b){ //eslint-disable-line
-//
-// }
-//
-// // Here is the test for multiply(); uncomment it to run it
-// // testMultiply(5,9);
-//
-// // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
-//
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
@@ -62,31 +38,7 @@ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
-// /////////////////////////////////////
-// /* Problem 3
-// Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
-//
-// Third element: "4 and 7 and 5 sum to 16."
-// Fourth element: "The product of 4 and 7 and 5 is 140."
-//
-// IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
-//
-// Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
-//
-// // Write your code here
-// function sumAndMultiply(a,b,c){ //eslint-disable-line
-//
-// }
-//
-// // Here is the test for sumAndMultiply(); uncomment it to run it
-// // testSumAndMultiply(4,7,5);
-//
-// // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
-//
-// /////////////////////////////////////
-
-//Note!! I somehow ended up trying to merge in the command line, which has given me this branch of master|MERGING for the last 10 or so minutes so I'm just editing in the text on the web. 
-
+/////////////////////////////////////
 /* Problem 3
 Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
 
